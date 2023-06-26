daa98c54228f7e69e0c874c1e272bb5777fb6f81
Carlos Diaz <freedancegrp@gmail.com>
2023-06-26 17:23:57 -0500

    PWA Bugfix

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,15 +1,42 @@
-window.self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-        return cache.addAll(['/', '/index.html', '/manifest.json', /* Agrega aquí otros archivos estáticos que deseas almacenar en caché */]);
-        })
-    );
+// Asigna un nombre único a la versión del service worker
+const CACHE_NAME = 'my-app-cache-v1';
+
+// Lista de archivos estáticos que se almacenarán en caché
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Agrega aquí otros archivos estáticos que deseas almacenar en caché
+];
+
+// Evento de instalación del service worker
+window.self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-  
-window.self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+
+// Evento de activación del service worker
+window.self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(cacheName => cacheName !== CACHE_NAME)
+            .map(cacheName => caches.delete(cacheName))
+        );
+      })
+  );
+});
+
+// Evento fetch para manejar las solicitudes de recursos
+window.self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
